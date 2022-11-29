2e2d83485d8f4d6d2019b84897ef4351eab5576d
Nick Mastoris <nmastoris@cti.gr>
2022-11-29 13:39:07 +0200

    replace distinguishedName with voPersonCertificateDN

--- attributemap/perunoid2oid.php
+++ attributemap/perunoid2oid.php
@@ -1,6 +1,6 @@
 <?php
 $attributemap = array(
 
-    // 'userCertificateSubject'=> 'distinguishedName',
-    'urn:oid:1.3.6.1.4.1.11433.2.2.1.9'  => 'urn:oid:2.5.4.49',
+    // 'userCertificateSubject'=> 'voPersonCertificateDN',
+    'urn:oid:1.3.6.1.4.1.11433.2.2.1.9'  => 'urn:oid:1.3.6.1.4.1.25178.4.1.3',
 );
